13a62639823ccb895d4c6f6b179d29c69917b644
Komputer <kowalczyk.przemek@wp.pl>
2025-05-17 13:38:31 +0200

    Initial commit

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -2,7 +2,7 @@ import type { MetaFunction } from "@remix-run/node";
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
+    { title: "CMS - Project" },
     { name: "description", content: "Welcome to Remix!" },
   ];
 };
@@ -12,7 +12,7 @@ export default function Index() {
     <div className="flex h-screen items-center justify-center">
       <div className="flex flex-col items-center gap-16">
         <header className="flex flex-col items-center gap-9">
-          <h1 className="leading text-2xl font-bold text-gray-800 dark:text-gray-100">
+          <h1 className="leading text-2xl font-bold text-pink-600 dark:text-gray-100">
             Welcome to <span className="sr-only">Remix</span>
           </h1>
           <div className="h-[144px] w-[434px]">
